fc583fecc66c13b8c719c7f85a4544b30708327e
TheRavenSeb <cdawgthomas@gmail.com>
2026-02-09 16:08:26 -0500

    Update asset path in charts.ejs and ensure static assets are served correctly

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
+app.use("/assets", express.static(path.join(__dirname, 'assets')));
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
