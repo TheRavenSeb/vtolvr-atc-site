cc65ce20ffda26639223c4fea23d407f07974195
TheRavenSeb <cdawgthomas@gmail.com>
2026-03-04 09:27:28 -0500

    fix: enhance admin role check to include session user

--- functions/AuthHandler.js
+++ functions/AuthHandler.js
@@ -68,7 +68,7 @@ async function register(username, password, email, role) {
 
 
 function AdminOnly(req, res, next) {
-    if (req.user && req.user.Role.includes('admin')) {
+    if (req.user && req.user.Role.includes('admin') || req.session.user && req.session.user.Role.includes('admin')) {
         return next();
     }
     res.status(403).json({ message: 'Forbidden' });
@@ -99,5 +99,5 @@ module.exports = {
     register,
     AdminOnly,
     restrict
-    
+
 };
