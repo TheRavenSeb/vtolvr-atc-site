1d5a90428dbda31c09a31fba222d481a3f6b7ced
TheRavenSeb <cdawgthomas@gmail.com>
2026-04-05 11:51:21 -0400

    fix: add missing ButtonStyle import in logFlightHours command

--- commands/logFlightHours.js
+++ commands/logFlightHours.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder,ActionRow, } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder,ActionRow,ButtonStyle,ButtonBuilder } = require('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
